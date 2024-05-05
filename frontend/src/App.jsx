9e0c2c830c3ec8b47538f067313d0ec2154b0bd4
samuelcody710 <adeyemis710@gmail.com>
2024-05-05 21:47:41 +0100

    cool

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
 
 
 
 export const UserContext = createContext({});
 
 const App = () => {
+    const [userAuth, setUserAuth] = useState({});
+    useEffect(() => {
+        let userInSession = lookInSession("user");
+        userInSession ?  setUserAuth(JSON.parse(userInSession)) : setUserAuth({access_token:null})
+    },[])
+
     return (
-        <UserContext.Provider>
+        <UserContext.Provider value={{userAuth, setUserAuth}}>
+            <div className="hidden "></div>
         <Routes>
             <Route path="/" element={<Navbar />}>
             <Route path="signin" element={<UserAuthForm  type="sign-in"/>}/>
